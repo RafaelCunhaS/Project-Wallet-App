a233c402c59231d31f5786bbfee6a5c918fdb82d
Rafael Santos <rafaelcs_94@hotmail.com>
2022-03-02 11:56:20 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
-  useEffect(() => {
-    document.title = 'Wallet App';
-  }, []);
-
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
